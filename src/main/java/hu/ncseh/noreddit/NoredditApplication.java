81791497562cc7d899795739d07ad4189f276ff6
NorbertCseh <norbert.cseh@protonmail.com>
2022-05-02 21:33:15 +0200

    init commit

---
+++ src/main/java/hu/ncseh/noreddit/NoredditApplication.java
@@ -0,0 +1,13 @@
+package hu.ncseh.noreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoredditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoredditApplication.class, args);
+	}
+
+}
